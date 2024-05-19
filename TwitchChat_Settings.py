6e474f86672413f6246fd8c3ce1c60999debb165
Aqusorias <Aqusorias@outlook.com>
2024-05-19 23:31:26 +0200

    Added more logging and colourization

--- TwitchChat_Settings.py
+++ TwitchChat_Settings.py
@@ -1,26 +1,31 @@
 import concurrent.futures
 import time
 import os
+from colorama import Fore, Back, Style, init        # colouring in the console
 import requests
 import keyboard         # killswitch
 import TwitchChat
+  
 
 
-# Remember to setup your Client_ID & Client_Secret & OAuth_Token in your environment variables if you want to get the User ID.
+# Remember to setup your Client_ID, Client_Secret & OAuth_Token in your environment variables if you want to get the User ID.
 CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
 CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
 oauth_token = os.getenv('TWITCH_OAUTH_TOKEN')
-    
-# Replace this with your Twitch username, if you have problems, try using the username in lowercase
-TWITCH_CHANNEL = 'otzdarva' 
+
+# If you want to get the User ID, set this to True, otherwise leave it on False.
+GetUserID = False
+
+# Replace this with the Twitch username you want to use, if you have problems, try using the username in lowercase
+TWITCH_CHANNEL = 'username' 
 
 # The lower the message, the faster the messages are processed: it's the number of seconds it will take to handle all messages in the queue.
 # Twitch delivers messages in batches, if set to 0 it will process it instantly, that's pretty bad if you have many messages incoming.
 # So if you don't have many messages, just leave it on 0.2.
 MESSAGE_RATE = 0.2
 
-# If you have a lot of messages, you can for example put in 10, so it will only process the first 10 messages of the queue/batch.
-# This won't be a problem if you aren't getting a lot of messages, so just leave it on 50.
+# If you have a lot of messages, you can for example put in 10, so it will only process the first 10 messages of the queue/batch, the rest will be deleted.
+# This won't be a problem if you aren't getting a lot of messages, so just leave it on 50.  Not recommended to set higher than 50.
 MAX_QUEUE_LENGTH = 50
 
 # Maximum number of messages it will process at the same time, just leave it on 100.
@@ -28,17 +33,46 @@ MAX_WORKERS = 100
 
 
 
+
 last_time = time.time()
 message_queue = []
 thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS)
 active_tasks = []
+init(autoreset=True)
 
-# Countdown before the bot starts
+
+if GetUserID == True:
+    if CLIENT_ID == None or CLIENT_SECRET == None or oauth_token == None:
+        if CLIENT_ID == None:
+            print(Fore.RED + 'Client ID is missing in the environment variables.')
+        if CLIENT_SECRET == None:
+            print(Fore.RED + 'Client Secret is missing in the environment variables.')
+        if oauth_token == None:
+            print(Fore.RED + 'OAuth Token is missing in the environment variables.')
+        print(Fore.YELLOW + 'Please set the environment variables for the script to work properly. Remember that after setting up the environment variables, you need to restart VSCode (your editor).')
+        exit()
+    else:
+        print(Fore.GREEN + 'Client ID: ' + Fore.BLUE + f'{CLIENT_ID[:5]}.......{CLIENT_ID[-3:]}', Fore.GREEN + 'Client Secret: ' + Fore.BLUE + f'{CLIENT_SECRET[:5]}.......{CLIENT_SECRET[-3:]}', Fore.GREEN + 'OAuth Token: ' + Fore.BLUE + f'{oauth_token[:5]}.......{oauth_token[-3:]}', sep='\n')
+
+            # If you are logging and want to see your hidden tokens, remove the comment before the print.
+            
+            # BE AWARE: DON'T DO THIS IF YOU STREAM OR RECORD!! IT WILL REVEAL YOUR TOKENS!!
+            # EVERYBODY THAT IS ABLE TO SEE YOUR SCREEN WILL KNOW YOUR TOKENS!!
+            
+        # print(Fore.GREEN + 'Client ID: ' + Fore.BLUE + f'{CLIENT_ID}', Fore.GREEN + 'Client Secret: ' + Fore.BLUE + f'{CLIENT_SECRET}', Fore.GREEN + 'OAuth Token: ' + Fore.BLUE + f'{oauth_token}', sep='\n')
+        
+        
+# Countdown before the script starts, upper the counter if you need to select a window before the program starts.
 countdown = 2
-while countdown > 0:
-    print(countdown)
-    countdown -= 1
-    time.sleep(1)
+print(' ')
+if countdown != 0:
+    print('Starting countdown ...')
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+        time.sleep(1)
+    print(' ')
+
 
 t = TwitchChat.Twitch()
 t.twitch_connect(TWITCH_CHANNEL)
@@ -61,23 +95,24 @@ def get_user_id(username, client_id, oauth_token):
             return data['data'][0]['id']
         else:
             return None
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching user ID for {username}: {e}")
+    except requests.exceptions.RequestException as exception:
+        print(Fore.RED + f"Error fetching user ID for {username}: " + Fore.YELLOW + str(exception))
         return None
-    
+
     
 # Remember that the Username is always lowercase (twitch standard).
 # It's better if you use the message at lowercase to avoid problems, but if neccecary, just remove the .lower().
 def handle_message(message):
     try:
-        msg = message['message'].lower()
+        msg = message['message']
         username = message['username'].lower()
-        user_id = get_user_id(username, CLIENT_ID, oauth_token)
-
-        if user_id:
+        
+        if GetUserID:
+            user_id = get_user_id(username, CLIENT_ID, oauth_token)
             print(f"{username} ({user_id}): {msg}")
         else:
-            print(f"{username} : {msg}")
+            print(f"{username}: {msg}")
+        
         
 
 ########################################## Add Rules ##########################################
@@ -85,18 +120,23 @@ def handle_message(message):
 
 
 
-        if msg == "hello":
-            print("User said Hello")
-            
-        if msg == "goodbye":
-            print("User said Goodbye")
+        # If you use msg.lower(), it will ignore the case of the message.
+
+        # If the message is exactly "hello"
+        if msg.lower() == "hello":
+            print(Fore.MAGENTA + "User said Hello")
+
+        # If message contains the word "hello"
+        if "hello" in msg.lower():
+            print(Fore.MAGENTA + "User said Hello")
 
 
 
 
 ########################################## <- Love you :3 -> ##########################################
-    except Exception as e:
-        print("Encountered exception: " + str(e))
+    except Exception as exception:
+        print(Fore.RED + "Encountered exception: " + Fore.YELLOW + str(exception))
+
 
 
 while True:
@@ -125,6 +165,10 @@ while True:
 
     # If User presses Shift+Backspace, automatically end the program - Killswitch
     if keyboard.is_pressed('shift+backspace'):
+        
+        print(' ')
+        print('\033[1m' + Back.YELLOW + Fore.RED + 'Program ended by user' + '\033[0m')
+        print(' ')
         exit()
         
 
@@ -135,5 +179,5 @@ while True:
             if len(active_tasks) <= MAX_WORKERS:
                 active_tasks.append(thread_pool.submit(handle_message, message))
             else:
-                print(f'WARNING: active tasks ({len(active_tasks)}) exceeds number of workers ({MAX_WORKERS}). ({len(message_queue)} messages in the queue)')
+                print(Back.YELLOW + Fore.RED + Style.BRIGHT + f'WARNING: active tasks ({len(active_tasks)}) exceeds number of workers ({MAX_WORKERS}). ({len(message_queue)} messages in the queue)')
  
